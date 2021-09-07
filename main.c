238167820a10c455d535e7f6f4c540d9f3b1650f
mQwand <85255383+mQwand@users.noreply.github.com>
2021-09-07 17:34:55 +0300

    main.c -> main.c + solvefunctions.c (.h)

--- main.c
+++ main.c
@@ -1,43 +1,12 @@
 #include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <time.h>
-
-
-const double NOT_ROOT = INFINITY;
-enum RANDOM {
-    RAND_PRECISION = 100000,
-    };
-enum ROOTS {
-    INFIN_ROOTS = -1,
-    };
-
-
-struct Equation {
-    double coef_a, coef_b, coef_c;
-    int roots_count;
-    double x1, x2;
-};
-
-/*! \brief Функция проверки числа на приближенность (равность) нулю.
- *
- * @param [in] number число с плавающей точкой, проверяемое на приближенность (равность) нулю.
- *
- * Функция принимает число типа double и проверяет его на конечность, после чего сравнивает его с константой PRECISION.
- *
- */
-
-
-int isApproxZero (double number) {
-    const double PRECISION = 0.0000005; /*!< константа, проверяющая допустимость погрешности нахождения корней */
-    if (!isfinite(number)) {
-        return 1;
-    }
-    return fabs(number) < PRECISION;
-}
+#include "solvefunctions.h"
 
 
 /*! \brief Функция генерации рандомного числа с плавающей точкой.
+ *
+ * @returns произвольное число с плавающей точкой
  *
  *  Функция использует функцию rand() из библиотеки <math.h> для получения рандомного числа типа int,
  *  после чего получает число с плавающей точкой и 6 знаками после точки приведением к типу (double) и делению на
@@ -49,100 +18,23 @@ double GetRandom () {
     return (double) rand() / (double) RAND_PRECISION;
 }
 
-
-/*! \brief Функция вывода корней уравнения и их количества.
- *
- * @param [in] x1 первый корень, число с плавающей точкой, которое выводится по необходимости.
- * @param [in] x2 второй корень, число с плавающей точкой, которое выводится по необходимости.
- * @param [in] roots_number целое число, количество корней решенного уравнения.
- *
- * Функция выводит полученные корни с округлением до 3 знаков после точки.
- *
- */
-
 void PrintResult (double x1, double x2, int roots_number) {
     switch (roots_number) {
         case INFIN_ROOTS:
             printf("Equation has infinite roots. \n");
             break;
 
-            case 0:
-                printf("Equation has no roots. \n");
-                break;
+        case 0:
+            printf("Equation has no roots. \n");
+            break;
 
-                case 1:
-                    printf("Equation has one root - %1.3lf. \n", x1);
-                    break;
+        case 1:
+            printf("Equation has one root - %1.3lf. \n", x1);
+            break;
 
-                    default:
-                        printf("Equation has two roots - %1.3lf and %1.3lf. \n", x1, x2);
-                        break;
-    }
-}
-
-
-/*! \brief Функция, решающая линейное уравнение.
- *
- * @param [in] b коэффициент уравнения b.
- * @param [in] c коэффициент уравнения c.
- * @param [out] x1 первый корень решаемого уравнения.
- *
- * Функция обновляет значения первого корня x1.
- *
- */
-
-int SolveLinearEquation (double b, double c, double* x1) {
-    if (isApproxZero(b)) {
-        if (isApproxZero(c)) {
-            *x1 = NOT_ROOT;
-            return INFIN_ROOTS;
-        } else {
-            return 0;
-        }
-    } else {
-        if (isApproxZero(c)) {
-            *x1 = 0;
-        } else {
-            *x1 = -c / b;
-        }
-        return 1;
-    }
-}
-
-
-/*! \brief Функция для решения квадратного уравнения.
- *
- * @param [in] coef_a коэффициент уравнения a.
- * @param [in] coef_b коэффициент уравнения b.
- * @param [in] coef_c коэффициент уравнения c.
- * @param [out] x1 первый корень решаемого уравнения.
- * @param [out] x2 второй корень решаемого уравнения.
- *
- * Вызывает функцию решения линейного уравнения при a = 0.
- * В остальных случаях решает квадратное уравнение стандартно: через дискриминант.
- *
- */
-
-int SolveEquation (double coef_a, double coef_b, double coef_c, double* x1, double* x2) {
-    double disc = 0;
-
-    if (isApproxZero(coef_a)) {
-        *x2 = NOT_ROOT;
-        return SolveLinearEquation(coef_b, coef_c, x1);
-    }
-
-    disc = coef_b * coef_b - 4 * coef_a * coef_c;
-    if (disc < 0) {
-        *x1 = *x2 = NOT_ROOT;
-        return 0;
-    } else if (isApproxZero(disc)) {
-        *x1 = (-coef_b) / (2 * coef_a);
-        *x2 = NOT_ROOT;
-        return 1;
-    } else {
-        *x1 = (-coef_b - sqrt(disc)) / (2 * coef_a);
-        *x2 = (-coef_b + sqrt(disc)) / (2 * coef_a);
-        return 2;
+        default:
+            printf("Equation has two roots - %1.3lf and %1.3lf. \n", x1, x2);
+            break;
     }
 }
 
@@ -153,6 +45,8 @@ int SolveEquation (double coef_a, double coef_b, double coef_c, double* x1, doub
  * @param [in] b коэффициент уравнения b.
  * @param [in] c коэффициент уравнения c.
  * @param [in] x1 первый корень решаемого уравнения.
+ * @returns 0 если переданный корень (x1) не является корнем уравнения или неточен.
+ * @returns 1 если переданный корень (x1) является корнем уравнения.
  *
  * Возвращает 1 или 0 в зависимости от правильности найденного корня. Является частью стресс теста.
  *
@@ -172,7 +66,7 @@ int CompleteTest(double x1, double a, double b, double c) {
 /*! \brief Функция выполнения стресс теста для заданного количества тестов.
  *
  * @param [in] stress_test_count количество выполняемых функцией стресс тестов.
- * @param [out] success_count количество успешно выполненных стресс тестов.
+ * @returns success_count количество успешно выполненных стресс тестов.
  *
  * Генерирует коэффициенты уравнений, изменяя сид генерации при каждом запуске.
  * Тестирует найденные корни на правильность с помощью функции CompleteTest().
@@ -196,20 +90,20 @@ int StressTest (int stress_test_count) {
                 success_count += CompleteTest(x1, random_a, random_b, random_c);
                 break;
 
-                case 2:
-                    success_count += CompleteTest(x1, random_a, random_b, random_c) &&
-                            CompleteTest(x1, random_a, random_b, random_c);
-                    break;
+            case 2:
+                success_count += CompleteTest(x1, random_a, random_b, random_c) &&
+                                 CompleteTest(x1, random_a, random_b, random_c);
+                break;
 
-                    case INFIN_ROOTS:
-                        case 0:
-                            // there is nothing to test for
-                            success_count++;
-                            break;
+            case INFIN_ROOTS:
+            case 0:
+                // there is nothing to test for
+                success_count++;
+                break;
 
-                            default:
-                                printf("Unexpected roots_number (Stress tests) - %d \n", roots_number);
-                                break;
+            default:
+                printf("Unexpected roots_number (Stress tests) - %d \n", roots_number);
+                break;
         }
     }
     return success_count;
@@ -219,7 +113,7 @@ int StressTest (int stress_test_count) {
 /*! \brief Функция выполнения юнит тестов для заданного (до запуска программы) количества тестов.
  *
  * @param [in] unit_test_count количество выполняемых функцией юнит тестов.
- * @param [out] success_count количество успешно выполненных юнит тестов.
+ * @returns success_count количество успешно выполненных юнит тестов.
  *
  * Решает уравнение для заданных коэффициентов. Проверяет соответствие найденных корней заданным вручную.
  *
@@ -246,34 +140,6 @@ int UnitTest (int unit_test_count, struct Equation unit_tests[unit_test_count][6
     return success_count;
 }
 
-
-/*! \brief Функция получения коэффициентов квадратного уравнения с клавиатуры.
- *
- * @param [out] coef_a коэффициент уравнения a.
- * @param [out] coef_b коэффициент уравнения b.
- * @param [out] coef_c коэффициент уравнения c.
- *
- */
-
-void GetCoefficients (double* coef_a, double* coef_b, double* coef_c) {
-    int success_scan = scanf("%lf %lf %lf", coef_a, coef_b, coef_c);
-    if (success_scan != 3) {
-        scanf("%*[^\n]");
-        printf("Please enter valid coefficients. \n");
-        GetCoefficients(coef_a, coef_b, coef_c);
-    }
-}
-
-
-/*! \brief Функция вывода приветственного сообщения с кратким описанием работы программы.
- *
- */
-
-void PrintStartingMessage () {
-    printf("This program is created to solve equations of the form ax² + bx + c = 0. \n"
-           "Please enter 3 coefficients of your equation in this order: a, b, c. \n"
-           "Remember that in a quadratic equation, the coefficient a must not be 0. \n");
-}
 
 
 /*! \brief Функция получения количества стресс тестов с клавиатуры.
@@ -328,21 +194,6 @@ void RunUnitTests () {
 }
 
 
-/*! \brief Функция запуска выполнения решения квадратного уравнения и всех необходимых функций.
- *
- */
-
-void RunEquationSolving () {
-    double coef_a = 0, coef_b = 0, coef_c = 0;
-    double x1 = 0, x2 = 0;
-
-    PrintStartingMessage();
-
-    GetCoefficients(&coef_a, &coef_b, &coef_c);
-    int roots_number = SolveEquation(coef_a, coef_b, coef_c, &x1, &x2);
-
-    PrintResult(x1, x2, roots_number);
-}
 
 
 /*! \brief Главная функция, задающая порядок выполнения и ход функций в программе.
---
+++ solvefunctions.c
@@ -0,0 +1,90 @@
+//
+// Created by Legion on 07.09.2021.
+//
+
+#include "solvefunctions.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+const double NOT_ROOT = INFINITY;
+
+
+
+int isApproxZero (double number) {
+    const double PRECISION = 0.0000005; /*!< константа, проверяющая допустимость погрешности нахождения корней */
+    if (!isfinite(number)) {
+        return 1;
+    }
+    return fabs(number) < PRECISION;
+}
+
+void PrintStartingMessage () {
+    printf("This program is created to solve equations of the form ax² + bx + c = 0. \n"
+           "Please enter 3 coefficients of your equation in this order: a, b, c. \n"
+           "Remember that in a quadratic equation, the coefficient a must not be 0. \n");
+}
+
+int SolveEquation (double coef_a, double coef_b, double coef_c, double* x1, double* x2) {
+    double disc = 0;
+
+    if (isApproxZero(coef_a)) {
+        *x2 = NOT_ROOT;
+        return SolveLinearEquation(coef_b, coef_c, x1);
+    }
+
+    disc = coef_b * coef_b - 4 * coef_a * coef_c;
+    if (disc < 0) {
+        *x1 = *x2 = NOT_ROOT;
+        return 0;
+    } else if (isApproxZero(disc)) {
+        *x1 = (-coef_b) / (2 * coef_a);
+        *x2 = NOT_ROOT;
+        return 1;
+    } else {
+        *x1 = (-coef_b - sqrt(disc)) / (2 * coef_a);
+        *x2 = (-coef_b + sqrt(disc)) / (2 * coef_a);
+        return 2;
+    }
+}
+
+int SolveLinearEquation (double b, double c, double* x1) {
+    if (isApproxZero(b)) {
+        if (isApproxZero(c)) {
+            *x1 = NOT_ROOT;
+            return INFIN_ROOTS;
+        } else {
+            return 0;
+        }
+    } else {
+        if (isApproxZero(c)) {
+            *x1 = 0;
+        } else {
+            *x1 = -c / b;
+        }
+        return 1;
+    }
+}
+
+void RunEquationSolving () {
+    double coef_a = 0, coef_b = 0, coef_c = 0;
+    double x1 = 0, x2 = 0;
+
+    PrintStartingMessage();
+
+    GetCoefficients(&coef_a, &coef_b, &coef_c);
+    int roots_number = SolveEquation(coef_a, coef_b, coef_c, &x1, &x2);
+
+    PrintResult(x1, x2, roots_number);
+}
+
+void GetCoefficients (double* coef_a, double* coef_b, double* coef_c) {
+    int success_scan = scanf("%lf %lf %lf", coef_a, coef_b, coef_c);
+    if (success_scan != 3) {
+        scanf("%*[^\n]");
+        printf("Please enter valid coefficients. \n");
+        GetCoefficients(coef_a, coef_b, coef_c);
+    }
+}
+
